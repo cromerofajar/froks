8e962da1838cd511a44d31c4b2e5328c7d2cd951
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-29 09:03:47 +0100

    Commit1

---
+++ src/forks/ForkS.java
@@ -0,0 +1,13 @@
+package forks;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class ForkS {
+    
+    public static void main(String[] args) {
+        System.out.println("****AÑADIR COMANDOS A PARTIR DE ESTE PUNTO.****");
+    }
+    
+}

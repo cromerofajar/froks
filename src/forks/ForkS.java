5777bdb043859918f146810b02dc758fdff7c224
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-29 09:32:01 +0100

    Commit2

--- src/forks/ForkS.java
+++ src/forks/ForkS.java
@@ -9,6 +9,7 @@ public class ForkS {
     public static void main(String[] args) {
         System.out.println("****AÑADIR COMANDOS A PARTIR DE ESTE PUNTO.****");
         System.out.println("Mi cambio en mi rama");
+        System.out.println("Ahora crea un cambio nuevo");
     }
     
 }
